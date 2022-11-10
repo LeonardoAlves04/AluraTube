cfd40c0290cba272b92b3b285da09f66601f0f69
Leonardo Alves <leonardoalves201510@gmail.com>
2022-11-10 13:34:56 -0300

    estilização do header

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,14 @@ const StyledHeader = styled.div`
     height: 80px;
     border-radius: 50%;
   }
+  .user-info{
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    widht: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
 `;
 
 function Header() {
