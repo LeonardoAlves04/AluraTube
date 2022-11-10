770ed9ef69a77c5e011232caebb37db0138e52cc
Leonardo Alves <leonardoalves201510@gmail.com>
2022-11-10 14:06:36 -0300

    inserindo banner

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset"
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline"
+import Banner from "../src/components/Banner";
 
 function HomePage() {
   console.log(config.playlists)
@@ -10,6 +11,7 @@ function HomePage() {
     <>
       <CSSReset />
       <div >
+        <Banner></Banner>
         <Menu></Menu>
         <Header></Header>
         <Timeline playlists={config.playlists}></Timeline>
---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import react from "react"
+
+function Banner(props) {
+    return (
+        <div>
+            <img src="aluratube/src/assets/banner-nuvem.jpg" alt="banner contendo nuvens"></img>
+        </div>
+
+    )
+}
+
+
+export default Banner 
