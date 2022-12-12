53394fb7405c0b15117100310731c1506aceebb3
Leonardo Alves <leonardoalves201510@gmail.com>
2022-12-12 16:33:10 -0300

    fix (): arrumado o bug de fetch que estava quebrando a aplicação, começando a criar a seção de favoritos

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import React, { useEffect } from "react"
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset"
-import Menu from "../src/components/Menu/";
+import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 import { videoService } from "../src/services/videoService";
-
 
 function HomePage() {
   const service = videoService();
@@ -13,91 +11,103 @@ function HomePage() {
   const [playlists, setPlaylists] = React.useState({});
 
   React.useEffect(() => {
-    console.log("useEffect")
-    service.getAllVideos()
-
+    console.log("useEffect");
+    service
+      .getAllVideos()
       .then((dados) => {
-        const novasPlaylists = { ...playlists };
+        console.log(dados.data);
+        // Forma imutavel
+        const novasPlaylists = {};
         dados.data.forEach((video) => {
           if (!novasPlaylists[video.playlist]) novasPlaylists[video.playlist] = [];
           novasPlaylists[video.playlist] = [
             video,
             ...novasPlaylists[video.playlist],
           ];
-          setPlaylists(novasPlaylists);
         });
-      }, []);
 
-    return (
-      <>
-        <div >
-          <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
-          <Header />
-          <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>Conteúdo</Timeline>
-        </div>
-      </>
-    )
-  }
+        setPlaylists(novasPlaylists);
+      });
+  }, []);
+
+  return (
+    <>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+      }}>
+        <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+        <Header />
+        <Timeline searchValue={valorDoFiltro} playlists={playlists}>
+          Conteúdo
+        </Timeline>
+      </div>
+    </>
+  );
+}
 
 export default HomePage
 
-  const StyledHeader = styled.div`
-  img {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-  }
-  .user-info{
-    display: flex;
-    align-items: center;
-    widht: 100%;
-    padding: 16px 32px;
-    gap: 16px;
-  }
+const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+`;
+const StyledBanner = styled.div`
+    background-color: blue;
+    background-image: url(${({ bg }) => bg});
+    /* background-image: url(${config.bg}); */
+    height: 230px;
 `;
 
-  const StyledBanner = styled.div`
-background-color: blue;
-background-image: url(${config.bg});
-height: 280px;
-background-repeat: no-repeat;
-background-size: cover;
-`;
+function Header() {
+  return (
+    <StyledHeader>
+      <StyledBanner bg={config.bg} />
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>
+            {config.name}
+          </h2>
+          <p>
+            {config.job}
+          </p>
+        </div>
+      </section>
+    </StyledHeader>
+  )
+}
 
-  function Header() {
-    return (
-      <StyledHeader>
-        <StyledBanner />
-        <section className="user-info">
-          <a href="https://github.com/LeonardoAlves04"><img src={`https://github.com/${config.github}.png`} alt="profile picture"></img></a>
-          <div>
-            <h2>
-              {config.name}
-            </h2>
-            <p>
-              {config.job}
-            </p>
-          </div>
-        </section>
-      </StyledHeader>
-    )
-  }
-
-  function Timeline({ searchValue, ...props }) {
-    const playlistNames = Object.keys(props.playlists);
-    return (
-      <StyledTimeline>
-        {playlistNames.map((playlistName) => {
-          const videos = props.playlists[playlistName];
-          return (
-            < section key={playlistName}>
-              <h2>{playlistName}</h2>
-              <div>
-                {videos.filter((video) => {
+function Timeline({ searchValue, ...propriedades }) {
+  const playlistNames = Object.keys(propriedades.playlists);
+  return (
+    <StyledTimeline>
+      {playlistNames.map((playlistName) => {
+        const videos = propriedades.playlists[playlistName];
+        return (
+          <section key={playlistName}>
+            <h2>{playlistName}</h2>
+            <div>
+              {videos
+                .filter((video) => {
                   const titleNormalized = video.title.toLowerCase();
                   const searchValueNormalized = searchValue.toLowerCase();
                   return titleNormalized.includes(searchValueNormalized)
-                }).map((video) => {
+                })
+                .map((video) => {
                   return (
                     <a key={video.url} href={video.url}>
                       <img src={video.thumb} />
@@ -107,12 +117,11 @@ background-size: cover;
                     </a>
                   )
                 })}
-              </div>
-            </section >
-          )
-        })}
-        <h2>Aqui vai a seção de favorites</h2>
-      </StyledTimeline>
-    )
-  }
-
+            </div>
+          </section>
+        )
+      })}
+      <h2>Aqui vai a seção de favorites</h2>
+    </StyledTimeline>
+  )
+}
